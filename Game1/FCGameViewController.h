417ec639b6b9e46d4922c7df5ebed6dee7b91e4c
ASPCartman <aspcartman@gmail.com>
2012-04-24 21:40:16 +0400

    Second Commit

---
+++ Game1/FCGameViewController.h
@@ -0,0 +1,12 @@
+//
+//  FCGameViewController.h
+//  Game1
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import "ASPEngine.h"
+@interface FCGameViewController : ASPGLKViewController
+
+@end
---
+++ Game1/FCGameViewController.m
@@ -0,0 +1,13 @@
+//
+//  FCGameViewController.m
+//  Game1
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import "FCGameViewController.h"
+
+@implementation FCGameViewController
+
+@end
