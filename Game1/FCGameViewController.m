ca08a598e8ffdbe7689a97261b2d66f15dee5d9c
Timofey Korchagin <tkorchagin@gmail.com>
2012-04-25 03:28:39 +0400

    Fix

--- Game1/FCGameViewController.m
+++ Game1/FCGameViewController.m
@@ -8,6 +8,25 @@
 
 #import "FCGameViewController.h"
 
-@implementation FCGameViewController
+@implementation FCGameViewController{
+    ASPGLSprite *first;
+}
+-(void) viewDidLoad{
+    [super viewDidLoad];
+    
+    first = [ASPGLSprite spriteWithTextureName:@"ball.png" effect: self.effect];
+    first.position = GLKVector2Make(self.viewIOSize.width/2, self.viewIOSize.height/2);
+    first.contentSize = CGSizeMake(10,10);
+    [self.sprites addObject:first];
+}
 
+#define A 50
+#define B 50
+#define a 3
+#define b 2
+#define q 0
+-(void)update{
+    static int t = 1;
+    first.position = GLKVector2Make(50*sin(2*t+0), 50*sin(3*t));
+}
 @end
