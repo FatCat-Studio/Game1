dfd020fb5b5c57b08b334f9d58f990980efe3066
ASPCartman <aspcartman@gmail.com>
2012-04-24 21:11:19 +0400

    First Commit

---
+++ Game1/FCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCAppDelegate.h
+//  Game1
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Game1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Game1
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FCAppDelegate class]));
+	}
+}
