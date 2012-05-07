8adb2a1d44dc075cc5551705237d61a8d64cba0c
ASPCartman <aspcartman@gmail.com>
2012-05-07 22:56:06 +0400

    Нахер внешний движок
    Не имеет смысла его хранить в отдельном проекте сейчас. Вернул его обратно, чуть чуть подправил. Проект ASPEngine можно удалять.

---
+++ Game1/ASPEngine.h
@@ -0,0 +1,10 @@
+//
+//  ASPEngine.h
+//  Demo
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+#include "ASPGLSprite.h"
+#include "ASPGLKVector2Extension.h"
+#include "ASPGLKViewController.h"
--- Game1/FCGameViewController.m
+++ Game1/FCGameViewController.m
@@ -23,12 +23,9 @@
 }
 -(void) viewDidLoad{
     [super viewDidLoad];
-    boysNumber = 0;
-    girlsNumber = 0;
     blackHole =[ASPGLSprite spriteWithTextureName:@"player.png" effect:self.effect]; //наша черная дыра под пальцем
 	blackHole.hidden = YES;
-    blackHole.contentSize = CGSizeMake(150,150);
-    w = 0;
+    blackHole.contentSize = CGSizeMake(100,100);
     for (int i=0; i<=BOYS+GIRLS; ++i){
         ASPGLSprite *sp;
         if(i<=BOYS){ // Если парень
@@ -44,7 +41,7 @@
         t[i]=(i*3.14)/(BOYS+GIRLS); // Задаем время отступа от нуля, чтобы они летали в разных фазах
         [self.sprites addObject:sp]; // Запихиваем в массив
     }
-       [self.sprites addObject:blackHole]; // Запихиваем в массив
+	[self.sprites addObject:blackHole]; // Запихиваем в массив
 }
 
 // Максимальная скорость по X:
@@ -55,36 +52,43 @@
 #define a 5
 // Количество раз пересечет экран по Y прямой:
 #define b 4
+
+- (void) blackHoleLogicUpdate:(ASPGLSprite*) sp{
+	blackHole.position = GLKVector2Make(touchPos.x, touchPos.y-blackHole.contentSize.height/2);
+	//blackHole.hidden = NO;
+	// Рассчитываем новую скорость шарика под действием силы притяжения Черной дыры
+	GLKVector2 vect = GLKVector2Subtract(touchPos, sp.centerPosition); // Вектор от черной дыры к шарику
+	int distance = sqrt(vect.x*vect.x+vect.y*vect.y); // Расстояние до черной дыры от шарика
+	vect = GLKVector2Normalize(vect); // Привели к единичной длине
+	CGFloat acceleration = 5;// Модуль силы
+	GLfloat width = sp.contentSize.width; // Ширина спрайта
+	GLfloat height = sp.contentSize.height; // Высота спрайта
+	if(distance < 50){
+		sp.contentSize = CGSizeMake(width-1, height-1);
+		acceleration = 20;
+	}else {
+		sp.contentSize = CGSizeMake(20, 20); //Если вылетели из круга и шар есть, то возвращаем размер
+	}
+	vect = GLKVector2MultiplyScalar(vect, acceleration); // Задали направление ускорения
+	sp.velocity = GLKVector2MultiplyScalar(vect, 20); // Новая скорость шарика
+	if(width+height<20){ //Если исчезли, то очистить память
+		if([sp.fileName compare:@"boy"]==0){
+			[boysField setText:[NSString stringWithFormat:@"%d", boysNumber++]];
+		} else {
+			[girlsField setText:[NSString stringWithFormat:@"%d", girlsNumber++]];
+		}
+		[sp outOfView];
+	}
+}
+
 -(void)update{
     for (int i = 0; i <= BOYS+GIRLS; ++i){
         ASPGLSprite* sp = [self.sprites objectAtIndex:i];
+		if (!sp) continue;
+		if (sp.hidden) continue;
         //Палец
         if(touching){
-            blackHole.position = GLKVector2Make(touchPos.x, touchPos.y-blackHole.contentSize.height/2);
-            blackHole.hidden = NO;
-            // Рассчитываем новую скорость шарика под действием силы притяжения Черной дыры
-            GLKVector2 vect = GLKVector2Subtract(touchPos, sp.centerPosition); // Вектор от черной дыры к шарику
-            int distance = sqrt(vect.x*vect.x+vect.y*vect.y); // Расстояние до черной дыры от шарика
-            vect = GLKVector2Normalize(vect); // Привели к единичной длине
-            CGFloat acceleration = 5;// Модуль силы
-            GLfloat width = sp.contentSize.width; // Ширина спрайта
-            GLfloat height = sp.contentSize.height; // Высота спрайта
-            if(distance < 70){
-                sp.contentSize = CGSizeMake(width-1, height-1);
-                acceleration = 20;
-            }else {
-                sp.contentSize = CGSizeMake(20, 20); //Если вылетели из круга и шар есть, то возвращаем размер
-            }
-            vect = GLKVector2MultiplyScalar(vect, acceleration); // Задали направление ускорения
-            sp.velocity = GLKVector2MultiplyScalar(vect, 20); // Новая скорость шарика
-            if(width+height<3){ //Если исчезли, то очистить память
-                if([sp.fileName compare:@"boy"]==0){
-                    [boysField setText:[NSString stringWithFormat:@"%d", boysNumber++]];
-                } else {
-                    [girlsField setText:[NSString stringWithFormat:@"%d", girlsNumber++]];
-                }
-                [sp outOfView];
-            }
+            [self blackHoleLogicUpdate:sp];
         }else{
             sp.contentSize = CGSizeMake(20, 20); // Если отпустили и шар есть, то возвращаем размер
             sp.velocity = GLKVector2Make(X*sin(a*t[i]+w), Y*sin(b*t[i]));
@@ -92,7 +96,7 @@
         }
         [sp update:self.timeSinceLastUpdate];
     }
-    [blackHole update:self.timeSinceLastUpdate];
+	[blackHole update:self.timeSinceLastUpdate];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
